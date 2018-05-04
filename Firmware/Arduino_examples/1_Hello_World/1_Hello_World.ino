4ae4b665354c6787b27e33c69e954c6da7c1c2c3
Ho Yun "Bobby <ho.chan@colorado.edu>
2018-05-03 20:37:01 -0600

    Update 1_Hello_World.ino

--- Firmware/Arduino_examples/1_Hello_World/1_Hello_World.ino
+++ Firmware/Arduino_examples/1_Hello_World/1_Hello_World.ino
@@ -10,29 +10,29 @@
 #include <SoftwareSerial.h>
 
 // Attach the serial enabld LCD's RX line to digital pin 11
-SoftwareSerial mySerial(10, 11); // Arduino SS_RX = pin 10 (unused), Arduino SS_TX = pin 11 
+SoftwareSerial LCD(10, 11); // Arduino SS_RX = pin 10 (unused), Arduino SS_TX = pin 11 
 
 void setup()
 {
-  mySerial.begin(9600); // set up serial port for 9600 baud
+  LCD.begin(9600); // set up serial port for 9600 baud
   delay(500); // wait for display to boot up
 }
 
 void loop()
 {
   // move cursor to beginning of first line
-  mySerial.write(254); 
-  mySerial.write(128);
+  LCD.write(254); 
+  LCD.write(128);
 
   // clear display by sending spaces
-  mySerial.write("                "); 
-  mySerial.write("                ");
+  LCD.write("                "); 
+  LCD.write("                ");
 
  // move cursor to beginning of first line
-  mySerial.write(254); 
-  mySerial.write(128);
+  LCD.write(254); 
+  LCD.write(128);
 
-  mySerial.write("Hello, world!");
+  LCD.write("Hello, world!");
 
   while(1); // wait forever
 }
