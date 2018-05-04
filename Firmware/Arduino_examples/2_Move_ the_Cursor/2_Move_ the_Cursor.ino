a68f4cde9faaf4e42dfde8be973e8c09397cf452
Ho Yun "Bobby <ho.chan@colorado.edu>
2018-05-03 20:38:02 -0600

    Update 2_Move_ the_Cursor.ino

--- Firmware/Arduino_examples/2_Move_ the_Cursor/2_Move_ the_Cursor.ino
+++ Firmware/Arduino_examples/2_Move_ the_Cursor/2_Move_ the_Cursor.ino
@@ -10,20 +10,20 @@
 #include <SoftwareSerial.h>
 
 // Attach the serial enabld LCD's RX line to digital pin 11
-SoftwareSerial mySerial(10, 11); // Arduino SS_RX = pin 10 (unused), Arduino SS_TX = pin 11
+SoftwareSerial LCD(10, 11); // Arduino SS_RX = pin 10 (unused), Arduino SS_TX = pin 11
 
 void setup()
 { 
-  mySerial.begin(9600); // set up serial port for 9600 baud
+  LCD.begin(9600); // set up serial port for 9600 baud
   delay(500); // wait for display to boot up
 
   // cursor to beginning of first line
-  mySerial.write(254); 
-  mySerial.write(128);
+  LCD.write(254); 
+  LCD.write(128);
 
   // clear display + legends
-  mySerial.write("RPM:            "); 
-  mySerial.write("TEMP:           ");
+  LCD.write("RPM:            "); 
+  LCD.write("TEMP:           ");
 } 
 
 int temp, rpm;
@@ -37,15 +37,15 @@ void loop()
   sprintf(tempstring,"%4d",rpm); // create strings from the numbers
   sprintf(rpmstring,"%4d",temp); // right-justify to 4 spaces
 
-  mySerial.write(254); // cursor to 7th position on first line
-  mySerial.write(134);
+  LCD.write(254); // cursor to 7th position on first line
+  LCD.write(134);
 
-  mySerial.write(rpmstring); // write out the RPM value
+  LCD.write(rpmstring); // write out the RPM value
 
-  mySerial.write(254); // cursor to 7th position on second line
-  mySerial.write(198);
+  LCD.write(254); // cursor to 7th position on second line
+  LCD.write(198);
 
-  mySerial.write(tempstring); // write out the TEMP value
+  LCD.write(tempstring); // write out the TEMP value
 
   delay(1000); // short delay
 }
